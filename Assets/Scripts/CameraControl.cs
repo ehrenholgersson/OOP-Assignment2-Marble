07b08549da235c7dd75bbb9f93cfcef32befb75c
ehrenholgersson <112737007+ehrenholgersson@users.noreply.github.com>
2023-11-16 21:59:51 +1100

    added cinemamachine

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour // Object for Cinemachine camera to follow
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.OnRaceStart += AttachCamera;
+    }
+
+    // Update is called once per frame
+    void AttachCamera()
+    {
+        if (GameController.PlayerMarble != null)
+        {
+            transform.position = GameController.PlayerMarble.transform.position;
+            transform.SetParent(GameController.PlayerMarble.transform, true);
+        }
+    }
+}
